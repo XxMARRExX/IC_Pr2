1cb83320545b921b7f61decff645b8a951779ae3
Tono3451 <116306863+Tono3451@users.noreply.github.com>
2024-11-26 13:08:46 +0000

    ejfbakfb

--- srf02_example/srf02_example.ino
+++ srf02_example/srf02_example.ino
@@ -14,11 +14,35 @@
  */
  
 #include <Wire.h> // Arduino's I2C library
- 
+
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// OLED display width and height in pixels
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+
+// SRF02's I2C address and command related definitions 
 #define SRF02_I2C_ADDRESS_1 byte((0xE0)>>1)
 #define SRFO2_I2C_ADDRESS_2 byte((0xE2)>>1) // DIRECCION POR DECIDIR ----------------
 #define SRF02_I2C_INIT_DELAY 100 // in milliseconds
 #define SRF02_RANGING_DELAY 70 // milliseconds
+#define tabulador "    "
 
 // LCD05's command related definitions
 #define COMMAND_REGISTER byte(0x00)
@@ -48,6 +72,7 @@ struct Sensor {
   int min_delay;
   int period_delay;
   bool shotting;
+  int last_shot;
 };
 
 Sensor sensor1;
@@ -77,10 +102,19 @@ byte read_register(byte address,byte the_register)
 void setup() 
 {
   Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
   
   Serial.println("initializing Wire interface ...");
   Wire.begin();
   delay(SRF02_I2C_INIT_DELAY);  
+
+  display.display();
+  delay(2000); // Pause for 2 seconds
   
   /*
   byte software_revision=read_register(SRF02_I2C_ADDRESS,SOFTWARE_REVISION);
@@ -90,6 +124,9 @@ void setup()
 
   init_sensors();
   unit_cm(sensor1);
+
+  //drawOLED();
+  prueba();
 }
 
 // the loop routine runs over and over again forever:
@@ -114,6 +151,53 @@ void loop()
 
 }
 
+void drawOLED(){
+  display.clearDisplay();
+
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.print(F("Sensor 1:"));
+  display.setTextColor(SSD1306_WHITE);
+  if (sensor1.shotting){
+    display.print(F(" on ")); display.print(F(sensor1.period_delay)); display.println(F(" ms"));
+  } else {
+    display.println(F(" off"));
+  }
+
+  display.display();
+}
+
+void prueba(){
+  drawSensorInformation(sensor1);
+  drawSensorInformation(sensor2);
+}
+
+void drawSensorInformation(Sensor& sensor){
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  if (sensor.dir_i2c == SRF02_I2C_ADDRESS_1){
+    display.print(F("Sensor 1:"));
+    if (sensor1.shotting){
+      display.print(F(" on ")); display.print(F(sensor1.period_delay)); display.println(F(" ms"));
+    } else {
+      display.println(F(" off"));
+    }
+
+
+  } else {
+    display.print(F("Sensor 2:"));
+    if (sensor2.shotting){
+      display.print(F(" on ")); display.print(F(sensor2.period_delay)); display.println(F(" ms"));
+    } else {
+      display.println(F(" off"));
+    }
+  }
+  
+  display.display();
+}
+
 //cosecha propia ------------------------------
 void init_sensors(){
   sensor1.address = SRF02_I2C_ADDRESS_1;
@@ -122,6 +206,7 @@ void init_sensors(){
   sensor1.min_delay = SRF02_RANGING_DELAY;
   sensor1.period_delay = 1000; // por poner algo
   sensor1.shotting = false;
+  sensor1.last_shot = 0;
 
   sensor2.address = SRFO2_I2C_ADDRESS_2;
   sensor2.unit = REAL_RANGING_MODE_INCHES;
@@ -129,6 +214,7 @@ void init_sensors(){
   sensor2.min_delay = SRF02_RANGING_DELAY;
   sensor2.period_delay = 1000; // por poner algo
   sensor2.shotting = false;
+  sensor2.last_shot = 0;
 }
 
 void unit_cm(Sensor& sensor){
