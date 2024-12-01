16cb6214172fc1a9fef12eddbbb72283939ff8d6
XxMARRExX <xx_marre_xx@hotmail.com>
2024-12-01 19:02:12 +0000

    Creacion comandos one-shot, unit, off

--- supervisor/comunicaciones_can.ino
+++ supervisor/comunicaciones_can.ino
@@ -1,7 +1,93 @@
-void oneshot_to_can(byte deviceAddrss){
+void oneshot_to_can(byte deviceAddrss) {
 
   byte data[2] = {0x00, deviceAddrss};
-  CAN.sendMsgBuf(0x200, 0, 2, data);
-  
+
+  if (CAN.sendMsgBuf(0x200, 0, 2, data) == CAN_OK) { 
+
+    //Serial.println("Mensaje enviado. Esperando ACK...");
+    if (waitForAck(deviceAddrss)) {
+      //Serial.println("Comunicación exitosa. ACK recibido.");
+    } else {
+      //Serial.println("Error: No se recibió ACK.");
+    }
+  } else {
+    //Serial.println("Error enviando mensaje.");
+  }
 }
 
+
+void us_off(byte deviceAddrss) {
+
+  byte data[2] = {0x01, deviceAddrss};
+
+  if (CAN.sendMsgBuf(0x200, 0, 2, data) == CAN_OK) { 
+
+    //Serial.println("Mensaje enviado. Esperando ACK...");
+    if (waitForAck(deviceAddrss)) {
+      //Serial.println("Comunicación exitosa. ACK recibido.");
+    } else {
+      //Serial.println("Error: No se recibió ACK.");
+    }
+  } else {
+    //Serial.println("Error enviando mensaje.");
+  }
+}
+
+// No implementado
+void us_on(byte deviceAddrss) {
+
+  byte data[2] = {0x01, deviceAddrss};
+
+  if (CAN.sendMsgBuf(0x200, 0, 2, data) == CAN_OK) { 
+
+    //Serial.println("Mensaje enviado. Esperando ACK...");
+    if (waitForAck(deviceAddrss)) {
+      //Serial.println("Comunicación exitosa. ACK recibido.");
+    } else {
+      //Serial.println("Error: No se recibió ACK.");
+    }
+  } else {
+    //Serial.println("Error enviando mensaje.");
+  }
+}
+
+
+void us_unit(byte unit, byte deviceAddrss) {
+
+  byte data[2] = {unit, deviceAddrss};
+
+  if (CAN.sendMsgBuf(0x200, 0, 2, data) == CAN_OK) { 
+
+    //Serial.println("Mensaje enviado. Esperando ACK...");
+    if (waitForAck(deviceAddrss)) {
+      //Serial.println("Comunicación exitosa. ACK recibido.");
+    } else {
+      //Serial.println("Error: No se recibió ACK.");
+    }
+  } else {
+    //Serial.println("Error enviando mensaje.");
+  }
+}
+
+
+bool waitForAck(byte expectedDeviceAddress) {
+
+  unsigned long startTime = millis(); 
+  while (millis() - startTime < 1000) { 
+    if (CAN.checkReceive() == CAN_MSGAVAIL) {
+      unsigned long id;
+      byte len;
+      byte buf[8];
+      CAN.readMsgBuf(&id, &len, buf);
+
+      if (id == 0x201 && len == 1) { // Verificar si es un ACK
+
+        if (buf[0] == expectedDeviceAddress) {
+          return true; 
+        }
+      }
+    }
+  }
+
+  return false; 
+}
--- supervisor/procesador_comandos.ino
+++ supervisor/procesador_comandos.ino
@@ -1,3 +1,8 @@
+#define dir1  0xFF // Tamaño máximo del comando
+#define us1   0x00 // Sensor ultrasonido1
+#define us2   0x01 // Sensor ultrasonido2
+#define oled  0x02 // Pantalla oled
+
 void processCommand(char* command) {
   Serial.print(F("Comando recibido: "));
   Serial.println(command);
@@ -33,41 +38,60 @@ void processCommand(char* command) {
 }
 
 
-void us_processor(char* token){
+int us_processor(char* token){
 
+  // 1. Comprobación del sensor del comando
   char* sensor_selct = token;
+  byte sensor = 0x00;
 
-  /*
-  Serial.print(F("US seleccionado "));
-  Serial.println(sensor);
-  */
+  if (strcmp(sensor_selct, "us1") == 0){
+    sensor = us1;
+  } else if(strcmp(sensor_selct, "us2") == 0){
+    sensor = us2;
+  }else {
+    Serial.println(F("Sensor no incorrecto"));
+    return -1;
+  }
 
   token = strtok(NULL, " ");
 
+  // 2. Función one-shot
   if(strcmp(token, "one-shot") == 0){
+    oneshot_to_can(sensor);
 
-    Serial.println(F(" one-shot."));
-
+  // 3. Función on
   }else if (strcmp(token, "on") == 0){
 
     Serial.println(F(" on."));
     token = strtok(NULL, " ");
 
+  // 4. Función off
   }else if (strcmp(token, "off") == 0){
+    us_off(sensor);
 
-    Serial.println(F(" off."));
-
-
+  // 5. Función unit
   }else if (strcmp(token, "unit") == 0){
-
-    Serial.println(F(" unit."));
     token = strtok(NULL, " ");
+    if (strcmp(token, "inc") == 0){
+      byte unit = 0x10;
+      us_unit(unit, sensor);
+    } else if(strcmp(token, "cm") == 0){
+      byte unit = 0x11;
+      us_unit(unit, sensor);
+    }else if(strcmp(token, "ms") == 0){
+      byte unit = 0x12;
+      us_unit(unit, sensor);
+    }else{
+      Serial.println(F("Unidad no reconocida"));
+    }
 
+  // 6. Función delay
   }else if (strcmp(token, "delay") == 0){
 
     Serial.println(F(" delay."));
     token = strtok(NULL, " ");
 
+  // 7. Función status
   }else if (strcmp(token, "status") == 0){
 
     Serial.println(F(" status."));
