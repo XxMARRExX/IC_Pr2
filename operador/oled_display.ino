8668111c6ae80a8dbfe528a8337ee76a692eedd0
XxMARRExX <xx_marre_xx@hotmail.com>
2024-12-09 12:01:30 +0000

    Modularizaión del código de Saúl

---
+++ operador/oled_display.ino
@@ -0,0 +1,71 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>Wire
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3D //< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// Declaraciones externas necesarias
+extern Adafruit_SSD1306 display;
+extern Sensor sensor1;
+extern Sensor sensor2;
+
+/**
+ * @brief Actualiza la pantalla OLED con información de los sensores.
+ */
+void drawOLED(){
+  Serial.print("drawOLED... ");
+  display.clearDisplay();
+
+  drawSensorInformationSensor1();
+  drawSensorInformationSensor2();
+  //drawCont();
+  Serial.println("done");
+}
+
+/**
+ * @brief Dibuja información del sensor 1 en la pantalla OLED.
+ */
+void drawSensorInformationSensor1() {
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.print(F("Sensor 1:"));
+  display.setTextColor(SSD1306_WHITE);
+  if (sensor1.shotting){
+    display.print(F(" on ")); display.print(sensor1.period_delay); display.println(F(" ms"));
+  } else {
+    display.println(F(" off"));
+  }
+
+  display.println();
+
+  display.print(F("Last shot: ")); display.println(sensor1.last_shot);
+
+  display.display();
+}
+
+/**
+ * @brief Dibuja información del sensor 2 en la pantalla OLED.
+ */
+void drawSensorInformationSensor2() {
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);        // Draw white text
+  int middleY = (64 - 8) / 2;
+  display.setCursor(0,middleY+4);             // Start at top-left corner
+  display.print(F("Sensor 2:"));
+  display.setTextColor(SSD1306_WHITE);
+  if (sensor2.shotting){
+    display.print(F(" on ")); display.print(sensor2.period_delay); display.println(F(" ms"));
+  } else {
+    display.println(F(" off"));
+  }
+
+  display.println();
+
+  display.print(F("Last shot: ")); display.println(sensor2.last_shot);
+
+  display.display();
+}
